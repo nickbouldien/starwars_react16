96fcef3cef94c4a5ef293920f703729c247d6e1b
nick bouldien <nick.bouldien@gmail.com>
2018-05-26 18:18:11 -0500

    commit before attempting to update react

--- src/routes/Frag.js
+++ src/routes/Frag.js
@@ -1,15 +1,15 @@
 import React, { Fragment } from 'react';
 import AnotherFragment from '../components/AnotherFragment';
 
-// const Frag = () => (
-//   <Fragment>
-//     Best Star Wars movie??
-//     {/* <AnotherFragment /> */}
-//   </Fragment>
-// );
-
 const Frag = () => (
-  "Best Star Wars movie??"
+  <Fragment>
+    Best Star Wars movie??
+    {/* <AnotherFragment /> */}
+  </Fragment>
 );
+
+// const Frag = () => (
+//   "Best Star Wars movie??"
+// );
 
 export default Frag;
